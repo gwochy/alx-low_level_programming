b7fb764efad99d69db9a8eeb2ad2dbaa2e246dd6
Prossy Gwokyalya <gwochy@gmail.com>
2023-02-17 08:51:04 -0800

    betty issue

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
+ * Description: main returns 0
+ *
  * Return - Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
