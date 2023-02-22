f36c9455793d3f420e7be7a5cea2a9ec612cad47
Prossy Gwokyalya <gwochy@gmail.com>
2023-02-22 07:02:09 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	scanf("%d %d %d", &a, &b, &c);
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 		/* printf("%d is the largest number\n", a);	*/
 	}
-	else if (b > a && b > c)
+	if (b >= a && b >= c)
 	{
 		largest = b;
 		/* printf("%d is the largest number\n", b);	*/
 	}
-	else
+	if (c >= a && c >= b)
 	{
 		largest = c;
 		/* printf("%d is the largest number\n", c);	*/
