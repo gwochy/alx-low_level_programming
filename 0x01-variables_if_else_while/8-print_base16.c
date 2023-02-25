a05f3092fd1423bb4a8e742c7b01d9f93f95bc9d
Prossy Gwokyalya <gwochy@gmail.com>
2023-02-25 09:07:07 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,11 +15,8 @@ int main(void)
 		putchar(48 + num); /* to return a char, 48 represents 0 */
 		num++;
 	}
-	while (num < 16)
-	{
-		putchar('%x');/* to return a char 59 represents 11 */
-		n++;
-	}
+	for (num  = 10; num < 16; num++)
+		putchar(87 + num);/* to return a char 97 represents 11 */
 	putchar('\n');
 	return (0);
 }
