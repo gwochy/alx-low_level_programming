efc9faa61e54ec248e435da0980949e2c175e967
Prossy Gwokyalya <gwochy@gmail.com>
2023-02-25 09:59:58 -0800

    single digit combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,14 @@ int main(void)
 {
 	int num = 0;
 
-	while (num < 10)
+	while (num < 9)
 	{
 		putchar(48 + num); /* to return a char, 48 represents 0 */
 		num++;
 		putchar(',');
-		putchar(' ');
 	}
+		if (num <= 9)
+			putchar(48 + num);
 	putchar('\n');
 	return (0);
 }
