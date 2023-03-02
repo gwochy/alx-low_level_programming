e6b11b8019bafc63fea6600a12a49d922d719dff
Prossy Gwokyalya <gwochy@gmail.com>
2023-03-02 13:06:52 -0800

    comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,25 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int res;
+/*	int res;	*/
+/*			*/
+/*	if (s1 < s2)	*/
+/*		res = -15;	*/
+/*	if (s1 > s2)	*/
+/*		res = 15;	*/
+/*	if (s1 == s2)		*/
+/*		res = 0;	*/
+/*				*/
+/*	return (res);		*/
 
-	if (s1 < s2)
-		res = -15;
-	if (s1 > s2)
-		res = 15;
-	if (s1 == s2)
-		res = 0;
+	int i;
 
-	return (res);
-
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
