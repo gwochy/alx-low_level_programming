f6ae89f8c323e1864b1493b0a36dc8b1b2bbf90c
Prossy Gwokyalya <gwochy@gmail.com>
2023-03-09 07:01:17 -0800

    Prints string by recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string by recursion
+ *
+ * @s: String to be printed
+ * Return: It returns s
+ */
+
+void _puts_recursion(char *s)
+{
+	if  (*(s) == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*(s));
+		_puts_recursion (s + 1);
+	}
+}
