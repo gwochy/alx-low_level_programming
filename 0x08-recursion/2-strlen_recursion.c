8d3e9ea67b3b049be83357488ac4610cdad8d23e
Prossy Gwokyalya <gwochy@gmail.com>
2023-03-09 09:09:05 -0800

    Counts the length of a string using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Counts the length of the string
+ *
+ * @s: the string in question
+ *Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (*(s) != '\0')
+	{
+		n = _strlen_recursion(s + 1);
+		return (1 + n);
+	}
+return 0;	
+}
